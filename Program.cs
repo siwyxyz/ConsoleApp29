33ee98e2567b1ab7fdb9d54e8ad3aa44503fcc9f
Yana Lashkevich <yana.lashkevich@zstbialystok.pl>
2022-12-05 14:30:03 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        class samochod
+        {
+            public string marka;
+            public string kolor;
+            public int cena;
+            public int data;
+
+            public samochod(string marka, int cena)
+            {
+                this.marka = marka;
+                this.cena = cena;
+            }
+            public samochod(string kolor, string marka, int cena, int data)
+            {
+                this.kolor = kolor;
+                this.marka = marka;
+                this.cena = cena;
+                this.data = data;
+            }
+            
+            
+
+            
+
+
+
+        }
+    }
+}
